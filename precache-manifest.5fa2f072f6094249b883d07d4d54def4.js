3d0d82b1da43ab52b7e4cb95357bbb6b8b043023
Sachin <sachin.b@zomato.com>
2019-07-10 00:42:36 +0530

    Updates

---
+++ precache-manifest.5fa2f072f6094249b883d07d4d54def4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e50f7c14e0215a956531ef4dbd353b",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "484fb22bbcf1ee427d22",
+    "url": "/react-tutorial/static/css/main.682ca21c.chunk.css"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "484fb22bbcf1ee427d22",
+    "url": "/react-tutorial/static/js/main.52ff816d.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  }
+]);
